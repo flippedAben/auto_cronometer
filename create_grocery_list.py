4c433878820ecacead2e3e071eabe805059f75b7
Ben Yang <ben.yang@utexas.edu>
2020-12-31 18:10:18 -0600

    Add scraping and data cleaning

---
+++ create_grocery_list.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import fractions
+from collections import defaultdict
+
+
+def info_multiple_versions(group_ing_df):
+    multiple_versions_exists = False
+    for key, df in group_ing_df:
+        df = df[['Unit', 'Recipe']]
+        if df.shape[0] > 1:
+            if df.index.nunique() > 1:
+                print('[Info] Multiple types of version per ingredient found.')
+                print()
+                multiple_versions_exists = True
+
+    if multiple_versions_exists:
+        for key, df in group_ing_df:
+            df = df[['Recipe']]
+            if df.shape[0] > 1:
+                if df.index.nunique() > 1:
+                    print(f'"[Info] {key}" has multiple versions:')
+                    print(df)
+                    print()
+
+
+def error_multiple_units(group_ing_df):
+    multiple_units_exists = False
+    for key, df in group_ing_df:
+        df = df[['Unit', 'Recipe']]
+        if df.shape[0] > 1:
+            if df['Unit'].nunique() > 1:
+                print('[Error] Multiple types of unit per ingredient found.')
+                print('[Error] Until I implement unit conversion, this will be an error.')
+                print()
+                multiple_units_exists = True
+                break
+
+    if multiple_units_exists:
+        for key, df in group_ing_df:
+            df = df[['Unit', 'Recipe']]
+            if df.shape[0] > 1:
+                if df.Unit.nunique() > 1:
+                    print(f'"[Error] {key}" has multiple units:')
+                    print(df)
+                    print()
+        print('[Error] Exiting...')
+        exit(1)
+
+
+def clean_data(all_ing_pd):
+    # Some amounts are in fractions (e.g. 1/2)
+    all_ing_pd[['Amount']] = all_ing_pd[['Amount']].applymap(lambda x:
+            float(fractions.Fraction(x)))
+    # Weight has the unit in it, so take it out...
+    all_ing_pd[['Weight']] = all_ing_pd[['Weight']].applymap(lambda x:
+            float(x.split(' ')[0]))
+    # ... and put it in the column name
+    all_ing_pd.rename(columns={'Weight': 'Weight (g)'}, inplace=True)
+
+
+all_ing_pd = pd.read_csv('data/ingredients.csv')
+clean_data(all_ing_pd)
+
+# Categorize ingredients (Description) by string up to the first comma
+all_ing_pd = all_ing_pd.set_index('Description')
+group_ing_df = all_ing_pd.groupby(lambda index: index.split(',')[0])
+
+# Ask user to use a single type of ingredient if possible
+info_multiple_versions(group_ing_df)
+
+# Ask user to only use one kind of unit per ingredient
+error_multiple_units(group_ing_df)
+
+# Create aggregated grocery list
+group_desc_df = all_ing_pd.groupby('Description')
+grocery_list = []
+for key, df in group_desc_df:
+    amount = round(pd.to_numeric(df['Amount']).sum(), 2)
+    unit = df['Unit'].unique()[0]
+    grocery_list.append([key, amount, unit, 0])
+
+groceries_df = pd.DataFrame(grocery_list)
+groceries_df.to_csv(
+    'data/grocery_list.csv',
+    header=['Name', 'Amount', 'Unit', 'Order'],
+    index=False)
---
+++ scrape.py
@@ -0,0 +1,108 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (
+    TimeoutException,
+    ElementClickInterceptedException,
+)
+import pandas as pd
+
+
+WAIT_SECONDS = 3
+
+
+def login(driver):
+    import creds
+    driver.get('https://cronometer.com/login/')
+    user_ele = driver.find_element_by_name('username')
+    pass_ele = driver.find_element_by_name('password')
+    user_ele.send_keys(creds.u)
+    pass_ele.send_keys(creds.p)
+
+    submit_button = driver.find_element_by_id('login-button')
+    submit_button.click()
+
+
+def robust_click(element, driver):
+    """
+    Sometimes the "Subscribe to Cronometer Gold" dialog pops up, which can
+    block a Selenium mouse click. If this will close that dialog before trying
+    to click the given element.
+    """
+    try:
+        element.click()
+    except ElementClickInterceptedException:
+        # close dialog
+        cancel_button = driver.find_element_by_class_name('GHDCC5OBEIC')
+        cancel_button.click()
+        print('Closed subscription popup dialog!')
+
+        # retry clicking the element
+        element.click()
+
+
+def scrape_recipe_ingredients(driver):
+    grocery_list_data = []
+    headers = []
+
+    try:
+        WebDriverWait(driver, WAIT_SECONDS).until(
+            EC.presence_of_element_located((By.ID, "navArea"))
+        )
+        print('Top level navigator loaded')
+        driver.get('https://cronometer.com/#foods')
+
+        WebDriverWait(driver, WAIT_SECONDS).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "gwt-DecoratedTabPanel"))
+        )
+        print('Food tab navigator loaded')
+        tabs = driver.find_elements_by_class_name('gwt-TabBarItem-wrapper')
+        for tab in tabs:
+            tab_label = tab.find_element_by_class_name('gwt-Label')
+            if tab_label.text.endswith('Recipes'):
+                robust_click(tab_label, driver)
+                break
+
+        recipe_div = driver.find_element_by_class_name('GHDCC5OBJRB')
+        recipes = recipe_div.find_elements_by_class_name('gwt-Anchor')
+        for recipe in recipes:
+            robust_click(recipe, driver)
+            WebDriverWait(driver, WAIT_SECONDS).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'admin-food-name'))
+            )
+            recipe_title = driver.find_element_by_class_name('admin-food-name')
+            print(f'{recipe_title.text} pane loaded')
+
+            ingredients = driver.find_element_by_class_name('GHDCC5OBPO')
+            ingredients_table_div = ingredients.find_element_by_class_name('GHDCC5OBKO')
+            ingredients_table = ingredients_table_div.find_elements_by_tag_name('tr')
+            if not headers:
+                headers = [
+                    td.text
+                    for td
+                    in ingredients_table[0].find_elements_by_tag_name('td')]
+                headers.append('Recipe')
+            for raw_row in ingredients_table[1:]:
+                row = [td.text for td in raw_row.find_elements_by_tag_name('td')]
+                row.append(recipe_title.text)
+                grocery_list_data.append(row)
+    except TimeoutException:
+        print('An element took too long to load. Re run the script.')
+
+    return grocery_list_data, headers
+
+# Enable headless Firefox
+import os
+os.environ['MOZ_HEADLESS'] = '1'
+
+print('Scraping ingredient information off Cronometer...')
+data = []
+headers = []
+with webdriver.Firefox(executable_path='./geckodriver') as driver:
+    login(driver)
+    data, headers = scrape_recipe_ingredients(driver)
+print()
+
+all_ing_pd = pd.DataFrame(data, columns=headers)
+all_ing_pd.to_csv('data/ingredients.csv')
